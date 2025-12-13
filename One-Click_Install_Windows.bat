0b849812a24f52071fdb9ab41bf0082f35a66264
cyberfantomo <cyberfantomo@proton.me>
2025-12-13 20:02:40 +0300

    Update One-Click_Install_Windows.bat

--- One-Click_Install_Windows.bat
+++ One-Click_Install_Windows.bat
@@ -1,37 +1,37 @@
 @echo off
-:: “áâ ­®¢¨âì ª®¤®¢ãî áâà ­¨æã ¤«ï ¯®¤¤¥à¦ª¨ àãááª¨å á¨¬¢®«®¢
+:: Установить кодовую страницу для поддержки русских символов
 :: Set code page to support Russian characters
 chcp 866 > nul
 
-:: “áâ ­®¢¨âì æ¢¥â
+:: Установить цвет
 :: Set color
 color 0A
 
-:: ‡ £®«®¢®ª
+:: Заголовок
 :: Headline
-echo â® One-Click “áâ ­®¢é¨ª
+echo Это One-Click Установщик
 echo This is a One-Click Installer
 echo.
 echo Privacy ^& Anonymity Patch v3 beta [06.2025]
 echo.
-echo Žä¨æ¨ «ì­ë© á ©â: www.cbf.st
+echo Официальный сайт: www.cbf.st
 echo Official Website: www.cbf.st
 echo.
-echo „«ï ®ãâ¥à :
+echo Для Роутера:
 echo For the Router:
 echo.
 echo CyberFantomo Anon4G+ Router [GL-E750/GL-E750V2]
 echo.
-echo ‚‚…„ˆ’… €Ž‹œ Ž’ €„Œˆ-€…‹ˆ ˆ €†Œˆ’… ENTER.
+echo ВВЕДИТЕ ПАРОЛЬ ОТ АДМИН-ПАНЕЛИ И НАЖМИТЕ ENTER.
 echo ENTER THE ADMIN PANEL PASSWORD AND PRESS ENTER.
-echo ‚ˆŒ€ˆ…! ‚¢®¤¨¬ë© ¯ à®«ì ­¥ ®â®¡à ¦ ¥âáï - íâ® áâ ­¤ àâ­ ï ¯à ªâ¨ª  ¤«ï ®¡¥á¯¥ç¥­¨ï ¡¥§®¯ á­®áâ¨.
+echo ВНИМАНИЕ! Вводимый пароль не отображается - это стандартная практика для обеспечения безопасности.
 echo ATTENTION! The entered password is not displayed - this is standard practice for security.
 echo.
 
 :: Remove known_hosts
 del "%USERPROFILE%\.ssh\known_hosts" /Q
 
-:: ®¤ª«îç¥­¨¥ ª à®ãâ¥àã ¨ ¢ë¯®«­¥­¨¥ ª®¬ ­¤
+:: Подключение к роутеру и выполнение команд
 :: Connecting to the router and executing commands
 ssh root@192.168.8.1 -o StrictHostKeyChecking=accept-new "wget -O decrypt_installer.sh https://raw.githubusercontent.com/cyberfantomo/CBFAR-PAP/refs/heads/main/decrypt_installer.sh && wget -O encrypted_installer.txt https://raw.githubusercontent.com/cyberfantomo/CBFAR-PAP/refs/heads/main/encrypted_installer.txt && chmod +x decrypt_installer.sh && ash decrypt_installer.sh"
 pause
